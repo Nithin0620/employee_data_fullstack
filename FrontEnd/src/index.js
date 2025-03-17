739b1252571744ba07871bc491c3ec0e6054f8ec
Nithin0620 <nithin30302@gmail.com>
2025-03-17 21:58:42 +0530

    FrontEnd Added

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import { ToastContainer, toast,Zoom } from 'react-toastify';
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <ToastContainer
+    position="top-center"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick={false}
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    transition={Zoom}
+    />
   </React.StrictMode>
 );
